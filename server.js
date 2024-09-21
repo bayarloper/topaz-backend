508a4a25d7ac31cafbe406a21e5301e74bd1beae
Bayardo <bayarloper@gmail.com>
2024-09-21 15:09:33 +0800

    soft launch

--- server.js
+++ server.js
@@ -10,14 +10,28 @@ const sections = require("./routes/sectionsRoutes")
 const doctorRoutes = require("./routes/doctorRoutes")
 const path = require("path");
 const bodyParser = require('body-parser');
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:3001', 'https://topaz-backend.vercel.app'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, Postman, etc.)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS')); // Deny the request
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(bodyParser.json());
 
